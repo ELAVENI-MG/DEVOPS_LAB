8a9e66aa8c8d617c03836516716e429136b3f9e6
Elaveni-mg <elavenimg@gmail.com>
2025-02-03 10:33:40 +0530

    first commit

---
+++ src/main/java/com/example1/demo4038/Demo4038Application.java
@@ -0,0 +1,13 @@
+package com.example1.demo4038;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4038Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4038Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example1/demo4038/Demo4038ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.demo4038;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo4038ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
